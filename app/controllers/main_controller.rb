7618a78a1280a1e0626878113ad33f5a3ee5353c
Emmanuel-Tembo <eman06tembo@gmail.com>
2026-01-14 15:25:30 +0200

    Flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,7 @@
 class MainController < ApplicationController
+  # the .now only displays on current request
   def index
-    
+    flash.now[:notice] = "Logged in successfully"
+    flash.now[:alert] = "Invalid email or password"
   end
 end
