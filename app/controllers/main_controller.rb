c1aafa74d2d674959ad2e031d7382f02ac763358
Emmanuel-Tembo <eman06tembo@gmail.com>
2026-01-15 21:35:59 +0200

    Log in with session cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,10 @@
 class MainController < ApplicationController
   # the .now only displays on current request
   def index
-    flash.now[:notice] = "Logged in successfully"
-    flash.now[:alert] = "Invalid email or password"
+    if session[:user_id]
+      # this looks up for the user and makes it available to the main controllers attributes
+      # use find_by instead of just find to avoid tedios errors/ crashing when user cannot be found
+      @user = User.find_by(session[:user_id])
+    end
   end
 end
--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -10,7 +10,8 @@ class RegistrationController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save 
-      
+      # sessions is a safer option due to it's encryption ability
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "Account Created successfully"
 
     else
