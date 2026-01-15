b407a8393f08ad61f66b95b5e5737cbb9d032ed7
Emmanuel-Tembo <eman06tembo@gmail.com>
2026-01-15 12:27:37 +0200

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   # Makes use of password digest to add a password and password confirmation that does not get addded to the database but bycrypts it
   has_secure_password
+
+  # Validations
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "must be a valid email address" }
 end
